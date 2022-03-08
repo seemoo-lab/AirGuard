773f6822cae99d13b73c75f6605dee6ee25d8543
Alexander Heinrich <aheinrich@seemoo.tu-darmstadt.de>
2022-03-08 14:46:47 +0100

    Detecting a tile device

--- app/src/main/java/de/seemoo/at_tracking_detection/database/models/device/types/Tile.kt
+++ app/src/main/java/de/seemoo/at_tracking_detection/database/models/device/types/Tile.kt
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothGatt
 import android.bluetooth.BluetoothGattCallback
 import android.bluetooth.BluetoothGattCharacteristic
 import android.bluetooth.le.ScanFilter
+import android.os.ParcelUuid
 import androidx.annotation.DrawableRes
 import de.seemoo.at_tracking_detection.ATTrackingDetectionApplication
 import de.seemoo.at_tracking_detection.R
@@ -14,7 +15,7 @@ import de.seemoo.at_tracking_detection.database.models.device.DeviceType
 import de.seemoo.at_tracking_detection.util.ble.BluetoothConstants
 import timber.log.Timber
 
-class Tile(val id: Int) : Device(), Connectable {
+class Tile(val id: Int) : Device(){
     override val imageResource: Int
         @DrawableRes
         get() = R.drawable.ic_baseline_device_unknown_24
@@ -26,41 +27,10 @@ class Tile(val id: Int) : Device(), Connectable {
     override val deviceContext: DeviceContext
         get() = Tile
 
-    override val bluetoothGattCallback: BluetoothGattCallback
-        get() = object : BluetoothGattCallback() {
-            override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
-                when (status) {
-                    BluetoothGatt.GATT_SUCCESS -> {
-                        when (newState) {
-                            else -> {
-                                Timber.d("Connection state changed to $newState")
-                            }
-                        }
-                    }
-                    else -> {
-                        Timber.e("Failed to connect to bluetooth device! Status: $status")
-                        broadcastUpdate(BluetoothConstants.ACTION_EVENT_FAILED)
-                    }
-                }
-            }
-
-            override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
-                super.onServicesDiscovered(gatt, status)
-            }
-
-            override fun onCharacteristicRead(
-                gatt: BluetoothGatt?,
-                characteristic: BluetoothGattCharacteristic?,
-                status: Int
-            ) {
-
-            }
-        }
-
     companion object : DeviceContext {
         // TODO: Implement scan filter for tile
         override val bluetoothFilter: ScanFilter
-            get() = ScanFilter.Builder().setDeviceAddress("FF:FF:FF:FF:FF:FF").build()
+            get() = ScanFilter.Builder().setServiceUuid(offlineFindingServiceUUID).build()
 
         override val deviceType: DeviceType
             get() = DeviceType.TILE
@@ -70,5 +40,7 @@ class Tile(val id: Int) : Device(), Connectable {
 
         override val statusByteDeviceType: UInt
             get() = 0u
+
+        val offlineFindingServiceUUID: ParcelUuid = ParcelUuid.fromString("FEED")
     }
 }
