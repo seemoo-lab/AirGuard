119fffbf12ded6ba60913d17b6ff93e9e4b171d0
Nishant <getrooted0019@hotmail.com>
2022-03-24 00:17:41 +0530

    Added bottom navigation dynamic coloring.

--- app/src/main/java/de/seemoo/at_tracking_detection/ui/MainActivity.kt
+++ app/src/main/java/de/seemoo/at_tracking_detection/ui/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.preference.PreferenceManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.elevation.SurfaceColors
 import dagger.hilt.android.AndroidEntryPoint
 import de.seemoo.at_tracking_detection.BuildConfig
 import de.seemoo.at_tracking_detection.R
@@ -26,6 +27,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Color the navigation bar the same color as bottom navigation.
+        window.navigationBarColor = SurfaceColors.SURFACE_2.getColor(this);
         val configuration = Configuration.getInstance()
         configuration.load(this, PreferenceManager.getDefaultSharedPreferences(this))
         setContentView(R.layout.activity_main)
