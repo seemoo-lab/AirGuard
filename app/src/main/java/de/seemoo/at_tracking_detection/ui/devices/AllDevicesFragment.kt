8124953e28b913c49e8ec622d2af4d27f815154a
Dennis Arndt <darndt@seemoo.tu-darmstadt.de>
2022-11-24 18:17:19 +0100

    smarttag and smarttag plus can be filtered separately

--- app/src/main/java/de/seemoo/at_tracking_detection/ui/devices/AllDevicesFragment.kt
+++ app/src/main/java/de/seemoo/at_tracking_detection/ui/devices/AllDevicesFragment.kt
@@ -91,7 +91,7 @@ class AllDevicesFragment : Fragment() {
         }
 
         view.findViewById<MaterialCardView>(R.id.smarttags_found_card).setOnClickListener {
-            val directions = AllDevicesFragmentDirections.actionNavigationAllDevicesFragmentToDevicesFound2(showDevicesFound = true, showAllDevices = true, deviceType = DeviceType.GALAXY_SMART_TAG) // TODO two types
+            val directions = AllDevicesFragmentDirections.actionNavigationAllDevicesFragmentToDevicesFound2(showDevicesFound = true, showAllDevices = true, deviceType = DeviceType.GALAXY_SMART_TAG, deviceType2 = DeviceType.GALAXY_SMART_TAG_PLUS)
             findNavController().navigate(directions)
         }
     }
