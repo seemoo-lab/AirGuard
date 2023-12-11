7696ca075f91aacd06a660c881f4b11bfad4a131
Dennis Arndt <dennis.arndt@stud.tu-darmstadt.de>
2023-12-11 14:35:23 +0100

    optimize BluetoothReceiver for Android 13 or higher

--- app/src/main/java/de/seemoo/at_tracking_detection/detection/BluetoothReceiver.kt
+++ app/src/main/java/de/seemoo/at_tracking_detection/detection/BluetoothReceiver.kt
@@ -5,6 +5,7 @@ import android.bluetooth.le.ScanResult
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import dagger.hilt.android.AndroidEntryPoint
 import de.seemoo.at_tracking_detection.database.repository.BeaconRepository
 import de.seemoo.at_tracking_detection.database.repository.DeviceRepository
@@ -23,8 +24,11 @@ class BluetoothReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         when (intent.action) {
             TrackingDetectorConstants.BLUETOOTH_DEVICE_FOUND_ACTION -> {
-                val scanResult =
-                    intent.getParcelableArrayListExtra<ScanResult>(BluetoothLeScanner.EXTRA_LIST_SCAN_RESULT)
+                val scanResult = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    intent.getParcelableArrayListExtra(BluetoothLeScanner.EXTRA_LIST_SCAN_RESULT, ScanResult::class.java)
+                } else {
+                    intent.getParcelableArrayListExtra(BluetoothLeScanner.EXTRA_LIST_SCAN_RESULT)
+                }
                 if (scanResult != null) {
                     for (result: ScanResult in scanResult) {
                         Timber.d("Found ${result.device.address}")
