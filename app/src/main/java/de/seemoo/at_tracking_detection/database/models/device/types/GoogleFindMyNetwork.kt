03cc5f0ffd9759f932cb13095b98d1559f89e4c5
Dennis Arndt <dennis.arndt@stud.tu-darmstadt.de>
2024-06-07 13:26:44 +0200

    implement Sound Service (not tested yet)

--- app/src/main/java/de/seemoo/at_tracking_detection/database/models/device/types/GoogleFindMyNetwork.kt
+++ app/src/main/java/de/seemoo/at_tracking_detection/database/models/device/types/GoogleFindMyNetwork.kt
@@ -1,19 +1,30 @@
 package de.seemoo.at_tracking_detection.database.models.device.types
 
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothProfile
 import android.bluetooth.le.ScanFilter
 import android.os.ParcelUuid
 import android.bluetooth.le.ScanResult
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import androidx.annotation.DrawableRes
 import de.seemoo.at_tracking_detection.ATTrackingDetectionApplication
 import de.seemoo.at_tracking_detection.R
+import de.seemoo.at_tracking_detection.database.models.device.Connectable
 import de.seemoo.at_tracking_detection.database.models.device.ConnectionState
 import de.seemoo.at_tracking_detection.database.models.device.Device
 import de.seemoo.at_tracking_detection.database.models.device.DeviceContext
 import de.seemoo.at_tracking_detection.database.models.device.DeviceType
 import de.seemoo.at_tracking_detection.util.Utility
+import de.seemoo.at_tracking_detection.util.ble.BluetoothConstants
 import timber.log.Timber
+import java.util.UUID
 
-class GoogleFindMyNetwork(val id: Int) : Device() {
+class GoogleFindMyNetwork(val id: Int) : Device(), Connectable {
 
     override val imageResource: Int
         @DrawableRes
@@ -26,7 +37,134 @@ class GoogleFindMyNetwork(val id: Int) : Device() {
     override val deviceContext: DeviceContext
         get() = GoogleFindMyNetwork
 
+    override val bluetoothGattCallback: BluetoothGattCallback
+        get() = object : BluetoothGattCallback() {
+            @SuppressLint("MissingPermission")
+            override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+                when (status) {
+                    BluetoothGatt.GATT_SUCCESS -> {
+                        when (newState) {
+                            BluetoothProfile.STATE_CONNECTED -> {
+                                Timber.d("Connected to gatt device!")
+                                gatt.discoverServices()
+                            }
+                            BluetoothProfile.STATE_DISCONNECTED -> {
+                                broadcastUpdate(BluetoothConstants.ACTION_GATT_DISCONNECTED)
+                                Timber.d("Disconnected from gatt device!")
+                            }
+                            else -> {
+                                Timber.d("Connection state changed to $newState")
+                            }
+                        }
+                    }
+                    else -> {
+                        Timber.e("Failed to connect to bluetooth device! Status: $status")
+                        broadcastUpdate(BluetoothConstants.ACTION_EVENT_FAILED)
+                    }
+                }
+            }
+
+            @SuppressLint("MissingPermission")
+            override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+                val uuids = gatt.services.map { it.uuid }
+                Timber.d("Found UUIDS $uuids")
+                val service = gatt.services.firstOrNull {
+                    it.uuid.toString().lowercase().contains(
+                        GOOGLE_SOUND_SERVICE.lowercase()
+                    )
+                }
+
+                if (service == null) {
+                    Timber.e("Playing sound service not found!")
+                    disconnect(gatt)
+                    broadcastUpdate(BluetoothConstants.ACTION_EVENT_FAILED)
+                    return
+                }
+
+                val characteristic = service.getCharacteristic(GOOGLE_SOUND_CHARACTERISTIC)
+
+                characteristic.let {
+                    gatt.setCharacteristicNotification(it, true)
+                    if (Build.VERSION.SDK_INT >= 33) {
+                        it.writeType
+                        gatt.writeCharacteristic(it, GOOGLE_START_SOUND_OPCODE, BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT)
+                    } else {
+                        // Deprecated since 33
+                        @Suppress("DEPRECATION")
+                        it.value = GOOGLE_START_SOUND_OPCODE
+                        @Suppress("DEPRECATION")
+                        gatt.writeCharacteristic(it)
+                    }
+                    Timber.d("Playing sound on Find My device with ${it.uuid}")
+                    broadcastUpdate(BluetoothConstants.ACTION_EVENT_RUNNING)
+                }
+            }
+
+            @SuppressLint("MissingPermission")
+            fun stopSoundOnGoogleDevice(gatt: BluetoothGatt) {
+                val service = gatt.services.firstOrNull {
+                    it.uuid.toString().lowercase().contains(
+                        GOOGLE_SOUND_SERVICE
+                    )
+                }
+
+                if (service == null) {
+                    Timber.d("Sound service not found")
+                    return
+                }
+
+                val uuid = GOOGLE_SOUND_CHARACTERISTIC
+                val characteristic = service.getCharacteristic(uuid)
+                characteristic.let {
+                    gatt.setCharacteristicNotification(it, true)
+                    if (Build.VERSION.SDK_INT >= 33) {
+                        it.writeType
+                        gatt.writeCharacteristic(it, GOOGLE_STOP_SOUND_OPCODE, BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT)
+                    } else {
+                        // Deprecated since 33
+                        @Suppress("DEPRECATION")
+                        it.value = GOOGLE_STOP_SOUND_OPCODE
+                        @Suppress("DEPRECATION")
+                        gatt.writeCharacteristic(it)
+                    }
+                    Timber.d("Stopping sound on Find My device with ${it.uuid}")
+                }
+            }
+
+            @SuppressLint("MissingPermission")
+            override fun onCharacteristicWrite(
+                gatt: BluetoothGatt?,
+                characteristic: BluetoothGattCharacteristic?,
+                status: Int
+            ) {
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+                    Timber.d("Finished writing to characteristic")
+                    if (characteristic?.value.contentEquals(GOOGLE_START_SOUND_OPCODE) && gatt != null) {
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            stopSoundOnGoogleDevice(gatt)
+                        }, 5000)
+                    }
+
+                    if (characteristic?.value.contentEquals(GOOGLE_STOP_SOUND_OPCODE)) {
+                        disconnect(gatt)
+                        broadcastUpdate(BluetoothConstants.ACTION_EVENT_COMPLETED)
+                    }
+
+                } else {
+                    Timber.d("Writing to characteristic failed ${characteristic?.uuid}")
+                    disconnect(gatt)
+                    broadcastUpdate(BluetoothConstants.ACTION_EVENT_FAILED)
+                }
+                super.onCharacteristicWrite(gatt, characteristic, status)
+            }
+        }
+
     companion object : DeviceContext {
+        internal const val GOOGLE_SOUND_SERVICE = "12F4"
+        internal val GOOGLE_SOUND_CHARACTERISTIC = UUID.fromString("8E0C0001-1D68-FB92-BF61-48377421680E")
+        internal val GOOGLE_START_SOUND_OPCODE = byteArrayOf(0x00, 0x03)
+        internal val GOOGLE_STOP_SOUND_OPCODE = byteArrayOf(0x01, 0x03)
+
         override val deviceType: DeviceType
             get() = DeviceType.GOOGLE_FIND_MY_NETWORK
 
