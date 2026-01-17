4bb00fd66bfca1b08949c52891fc471505fe8378
Dennis Arndt <dennis.arndt@stud.tu-darmstadt.de>
2026-01-17 13:04:19 +0100

    update gradle to 9.0, Material3 for All Devices Fragment

--- app/src/main/java/de/seemoo/at_tracking_detection/ui/devices/AllDevicesFragment.kt
+++ app/src/main/java/de/seemoo/at_tracking_detection/ui/devices/AllDevicesFragment.kt
@@ -1,12 +1,12 @@
 package de.seemoo.at_tracking_detection.ui.devices
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
